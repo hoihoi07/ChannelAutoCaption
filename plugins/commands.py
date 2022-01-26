05e80fe7ec9d195577c02364c38e353c15ce39db
hoihoi07 <97010331+hoihoi07@users.noreply.github.com>
2022-01-26 17:13:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ start_button=InlineKeyboardMarkup(
                   InlineKeyboardButton("About Dynamic", callback_data = "dynamic_data")
               ],
               [
-                  InlineKeyboardButton("Source Code", url = "https://github.com/samadii/ChannelAutoCaption"),
+                  InlineKeyboardButton("Channel", url = "https://t.me/jetbots"),
                   InlineKeyboardButton("🔐 CLOSE", callback_data="close_data")
               ]
         ]
